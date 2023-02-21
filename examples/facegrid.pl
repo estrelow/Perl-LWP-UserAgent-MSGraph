f48246788ab8795c9f6e79051e3db1f157464fe6
Erich Strelow <esf@moller.cl>
2023-02-21 15:09:07 -0300

    Begin to add the facegrid sample

---
+++ examples/facegrid.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Mojolicious::Lite -signatures;
+
+use constant APPID => '';
+use constant SECRET => '';
+use const TENANT => '';
+
+get '/' => sub ($c) {
+   
+}
+
+
+
